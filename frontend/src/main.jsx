343936c71a65c5761176e8766304a443cb5a51ec
Ashwin Balaraman <abalaraman1@uchicago.edu>
2024-11-27 23:25:20 -0600

    Finished adding some images and made it grid styles. Now have to normalize images  and some more

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import BazaarMatrix from './components/bazaarMatrix'
+import "./global.css"
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
